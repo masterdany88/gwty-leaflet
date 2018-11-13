b96610bcce167e3f0ca87e23d28658c0b7da3552
Daniel Korbel <daniel.korbel.ext@deposit-solutions.com>
2018-11-13 23:27:28 +0100

    Updated namespace to global

--- src/main/java/com/gwidgets/api/leaflet/QrScanner.java
+++ src/main/java/com/gwidgets/api/leaflet/QrScanner.java
@@ -4,7 +4,7 @@ import jsinterop.annotations.JsType;
 import elemental2.dom.Element;
 import jsinterop.annotations.JsPackage;
 
-@JsType(isNative = true)
+@JsType(namespace=JsPackage.GLOBAL, isNative=true)
 public class QrScanner {
     public QrScanner(Element element, Decode onDecode) {};
 	//public QrScanner(Element elementById, Object onDecode) {}
